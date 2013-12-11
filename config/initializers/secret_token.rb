06bbb5e495eeaab388c7d68c571634e4cd53325b
Marco <marcooliveira@infojr.com.br>
2013-12-11 16:13:33 -0300

    Commit inicial. Criado a estrutura basica de Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Traineeme::Application.config.secret_key_base = 'fd0e251ace0e29e196c9722ad261b6e333efdc23f70354fb6457ff62866b038e592d5739ccbf33e674a59e66aceab84264885b9f795d4644487a3bfc5fa0fa62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Traineeme::Application.config.session_store :cookie_store, key: '_traineeme_session'
